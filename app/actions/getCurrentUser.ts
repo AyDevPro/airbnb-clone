b5116732b71c6ae0572bd446007d1a59b86529cb
Aymeric <aymeric.surre@gmail.com>
2025-01-21 15:01:02 +0100

    Enhance authentication flow by adding LoginModal and integrating current user state. Updated NextAuth configuration for improved sign-in page routing and added user menu options based on authentication status.

---
+++ app/actions/getCurrentUser.ts
@@ -0,0 +1,28 @@
+import prisma from "@/app/libs/prismadb";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+
+export async function getSession() {
+    return await getServerSession(authOptions);
+}
+
+export async function getCurrentUser() {
+    try {   
+        const session = await getSession();
+        if (!session?.user?.email) {
+            return null;
+        }
+
+        const currentUser = await prisma.user.findUnique({
+            where: {
+                email: session.user.email as string,
+            },
+        });
+        if (!currentUser) {
+            return null;
+        }
+        return currentUser;
+    } catch (error: any) {
+        return null;
+    }
+}
